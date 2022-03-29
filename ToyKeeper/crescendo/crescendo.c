f4c2b3d4a634c39b3149b627fad4ba37e90a2658
willjow <williamjow67@gmail.com>
2022-03-29 13:46:59 -0700

    Move goodnight mode to its own function

--- ToyKeeper/crescendo/crescendo.c
+++ ToyKeeper/crescendo/crescendo.c
@@ -510,6 +510,41 @@ inline void poweroff() {
     sleep_mode();
 }
 
+inline void goodnight_mode() {
+    uint8_t i, j;
+    // signal that this is *not* the STEADY mode
+    blink(2, BLINK_SPEED/16);
+    #define GOODNIGHT_TOP (RAMP_SIZE/6)
+    // ramp up instead of going directly to the top level
+    // (probably pointless in this UI)
+    /*
+    for (i=1; i<=GOODNIGHT_TOP; i++) {
+        set_mode(i);
+        _delay_4ms(2*RAMP_TIME/RAMP_SIZE/4);
+    }
+    */
+    // ramp down over about an hour
+    for(i=GOODNIGHT_TOP; i>=1; i--) {
+        set_mode(i);
+        // how long the down ramp should last, in seconds
+        #define GOODNIGHT_TIME 60*60
+        // how long does _delay_s() actually last, in seconds?
+        // (calibrate this per driver, probably)
+        #define ONE_SECOND 1.03
+        #define GOODNIGHT_STEPS (1+GOODNIGHT_TOP)
+        #define GOODNIGHT_LOOPS (uint8_t)((GOODNIGHT_TIME) / ((2*ONE_SECOND) * GOODNIGHT_STEPS))
+        // NUM_LOOPS = (60*60) / ((2*ONE_SECOND) * (1+MODE_LOW-MODE_MOON))
+        // (where ONE_SECOND is how many seconds _delay_s() actually lasts)
+        // (in my case it's about 0.89)
+        for(j=0; j<GOODNIGHT_LOOPS; j++) {
+            _delay_s();
+            _delay_s();
+            //_delay_ms(10);
+        }
+    }
+    poweroff();
+}
+
 int main(void)
 {
     hw_setup();
@@ -759,38 +794,7 @@ int main(void)
         #ifdef GOODNIGHT
         // "good night" mode, slowly ramps down and shuts off
         else if (mode == GOODNIGHT) {
-            uint8_t i, j;
-            // signal that this is *not* the STEADY mode
-            blink(2, BLINK_SPEED/16);
-            #define GOODNIGHT_TOP (RAMP_SIZE/6)
-            // ramp up instead of going directly to the top level
-            // (probably pointless in this UI)
-            /*
-            for (i=1; i<=GOODNIGHT_TOP; i++) {
-                set_mode(i);
-                _delay_4ms(2*RAMP_TIME/RAMP_SIZE/4);
-            }
-            */
-            // ramp down over about an hour
-            for(i=GOODNIGHT_TOP; i>=1; i--) {
-                set_mode(i);
-                // how long the down ramp should last, in seconds
-                #define GOODNIGHT_TIME 60*60
-                // how long does _delay_s() actually last, in seconds?
-                // (calibrate this per driver, probably)
-                #define ONE_SECOND 1.03
-                #define GOODNIGHT_STEPS (1+GOODNIGHT_TOP)
-                #define GOODNIGHT_LOOPS (uint8_t)((GOODNIGHT_TIME) / ((2*ONE_SECOND) * GOODNIGHT_STEPS))
-                // NUM_LOOPS = (60*60) / ((2*ONE_SECOND) * (1+MODE_LOW-MODE_MOON))
-                // (where ONE_SECOND is how many seconds _delay_s() actually lasts)
-                // (in my case it's about 0.89)
-                for(j=0; j<GOODNIGHT_LOOPS; j++) {
-                    _delay_s();
-                    _delay_s();
-                    //_delay_ms(10);
-                }
-            }
-            poweroff();
+            goodnight_mode();
         }
         #endif // ifdef GOODNIGHT
 
