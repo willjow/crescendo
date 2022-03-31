84c8063b533fb73befb20db35840178144a32e4d
willjow <williamjow67@gmail.com>
2022-03-31 15:24:55 -0700

    Add preprocessor guards for strobe functions

--- ToyKeeper/crescendo/strobes.h
+++ ToyKeeper/crescendo/strobes.h
@@ -1,9 +1,13 @@
 #ifndef STROBES_H
 #define STROBES_H
 
+#ifdef ANY_STROBE
 void strobe(uint8_t ontime, uint8_t offtime);
+#endif
+#ifdef PARTY_STROBES
 void party_strobe(uint8_t ontime, uint8_t offtime);
 void party_strobe_loop(uint8_t ontime, uint8_t offtime);
+#endif
 
 #endif
 
