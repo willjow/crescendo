77e2e23a5186005fe4d3a7359bba102b2c13a9a4
willjow <williamjow67@gmail.com>
2022-03-31 15:23:59 -0700

    Remove SOS/biking declarations from `strobes.h`

--- ToyKeeper/crescendo/strobes.h
+++ ToyKeeper/crescendo/strobes.h
@@ -4,8 +4,6 @@
 void strobe(uint8_t ontime, uint8_t offtime);
 void party_strobe(uint8_t ontime, uint8_t offtime);
 void party_strobe_loop(uint8_t ontime, uint8_t offtime);
-void SOS_mode();
-void biking_mode(uint8_t lo, uint8_t hi);
 
 #endif
 
