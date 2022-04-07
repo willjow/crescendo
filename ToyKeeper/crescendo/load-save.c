fb3e5cabc23b44fb2d08b520623edc5c8e62eab4
willjow <williamjow67@gmail.com>
2022-04-07 12:15:01 -0700

    Maximize EEPROM wear-leveling size Increases the program size a bit, but also implements some enums to maybe (slightly) help with the maintainability.

--- ToyKeeper/crescendo/load-save.c
+++ ToyKeeper/crescendo/load-save.c
@@ -3,16 +3,6 @@
 
 #include "load-save.h"
 
-#if defined(MEMORY) || defined(CONFIG_MODE)
-#if (ATTINY == 85) || (ATTINY == 45)
-#define EEP_WEAR_LVL_LEN 128
-#elif (ATTINY == 25)
-#define EEP_WEAR_LVL_LEN 64
-#elif (ATTINY == 13)
-#define EEP_WEAR_LVL_LEN 32
-#endif
-#endif
-
 #ifdef MEMORY
 void save_mode() {  // save the current mode index (with wear leveling)
     #ifdef MEMTOGGLE
@@ -22,28 +12,27 @@ void save_mode() {  // save the current mode index (with wear leveling)
     {
         eeprom_write_byte((uint8_t *)(eepos), 0xff);     // erase old state
         eeprom_write_byte((uint8_t *)(++eepos), 0xff);     // erase old state
-
-        eepos = (eepos+1) & (EEP_WEAR_LVL_LEN-1);  // wear leveling, use next cell
+        eepos++;
+        if (eepos > EEP_WL_END)
+            eepos = EEP_WL_START;
         // save current mode
         eeprom_write_byte((uint8_t *)(eepos), mode_idx);
         // save current brightness
-        eeprom_write_byte((uint8_t *)(eepos+1), ramp_level);
+        eeprom_write_byte((uint8_t *)(eepos + 1), ramp_level);
     }
 }
 #endif
 
 #ifdef CONFIG_MODE
-#define OPT_memory (EEP_WEAR_LVL_LEN+1)
-#define OPT_therm_ceil (EEP_WEAR_LVL_LEN+2)
 void save_state() {
     #ifdef MEMORY
     save_mode();
     #endif
     #ifdef MEMTOGGLE
-    eeprom_write_byte((uint8_t *)OPT_memory, memory);
+    eeprom_write_byte((uint8_t *)EEP_MEMORY, memory);
     #endif
     #ifdef THERM_CALIBRATION_MODE
-    eeprom_write_byte((uint8_t *)OPT_therm_ceil, therm_ceil);
+    eeprom_write_byte((uint8_t *)EEP_THERM_CEIL, therm_ceil);
     #endif
 }
 #endif
@@ -54,14 +43,14 @@ void restore_state() {
     #ifdef MEMTOGGLE
     // memory is either 1 or 0
     // (if it's unconfigured, 0xFF, assume it's off)
-    eep = eeprom_read_byte((uint8_t *)OPT_memory);
+    eep = eeprom_read_byte((uint8_t *)EEP_MEMORY);
     if (eep < 2) { memory = eep; }
     else { memory = 0; }
     #endif
 
     #ifdef THERM_CALIBRATION_MODE
     // load therm_ceil
-    eep = eeprom_read_byte((uint8_t *)OPT_therm_ceil);
+    eep = eeprom_read_byte((uint8_t *)EEP_THERM_CEIL);
     if ((eep > 0) && (eep < MAX_THERM_CEIL)) {
         therm_ceil = eep;
     }
@@ -69,11 +58,11 @@ void restore_state() {
 
     #ifdef MEMORY
     // find the mode index and last brightness level
-    for(eepos=0; eepos<EEP_WEAR_LVL_LEN; eepos+=2) {
+    for (eepos = EEP_WL_START; eepos <= EEP_WL_END; eepos += 2) {
         eep = eeprom_read_byte((const uint8_t *)eepos);
         if (eep != 0xff) {
             saved_mode_idx = eep;
-            eep = eeprom_read_byte((const uint8_t *)(eepos+1));
+            eep = eeprom_read_byte((const uint8_t *)(eepos + 1));
             if (eep != 0xff) {
                 saved_ramp_level = eep;
             }
--- ToyKeeper/crescendo/load-save.h
+++ ToyKeeper/crescendo/load-save.h
@@ -1,6 +1,28 @@
 #ifndef LOAD_SAVE_H
 #define LOAD_SAVE_H
 
+enum eeprom_options {
+#ifdef MEMTOGGLE
+    EEP_MEMORY,
+#endif
+#ifdef THERM_CALIBRATION_MODE
+    EEP_THERM_CEIL,
+#endif
+    EEP_OPTIONS_END
+};
+
+enum eeprom_wl_options {
+#ifdef MEMORY
+    EEP_WL_MODE_IDX,
+    EEP_WL_RAMP_LEVEL,
+#endif
+    EEP_WL_OPTIONS_END
+};
+
+#define EEP_WL_SIZE (EEPSIZE - EEP_OPTIONS_END)
+#define EEP_WL_START EEP_OPTIONS_END
+#define EEP_WL_END (EEP_WL_START + EEP_WL_SIZE - EEP_WL_OPTIONS_END)
+
 #ifdef MEMORY
 void save_mode();
 #endif
