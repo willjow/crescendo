031bd0a4b01d696f59dae80115d4236b804428f5
willjow <williamjow67@gmail.com>
2023-03-27 12:16:56 -0700

    Clean up comments in `voltage-monitor.c` no need to keep a bunch of out-of-date, commented-out code

--- ToyKeeper/crescendo/voltage-monitor.c
+++ ToyKeeper/crescendo/voltage-monitor.c
@@ -24,52 +24,46 @@ void battcheck_mode() {
 
 #ifdef VOLTAGE_PROTECTION
 void monitor_voltage(uint8_t mode, uint8_t *lowbatt_cnt) {
-    //if (ADCSRA & (1 << ADIF)) {  // if a voltage reading is ready
-    {  // nope, always execute
-        //uint8_t voltage = ADCH;  // get the waiting value
-        VOLTAGE_TYPE voltage = get_voltage();  // get a new value, first is unreliable
-        // See if voltage is lower than what we were looking for
-        #ifdef VCC_REF
-        if (voltage > ADC_LOW)
-        #else
-        if (voltage < ADC_LOW)
-        #endif
-            (*lowbatt_cnt)++;
-        else
-            *lowbatt_cnt = 0;
+    // Always get a new value; the initial one is unreliable
+    VOLTAGE_TYPE voltage = get_voltage();
 
-        // See if it's been low for a while, and maybe step down
-        if (*lowbatt_cnt >= 8) {
-            // DEBUG: blink on step-down:
-            //set_level(0);  _delay_ms(100);
+    // See if voltage is lower than what we were looking for
+    #ifdef VCC_REF
+    if (voltage > ADC_LOW)
+    #else
+    if (voltage < ADC_LOW)
+    #endif
+        (*lowbatt_cnt)++;
+    else
+        *lowbatt_cnt = 0;
 
-            if (mode != STEADY_E) {
-                // step "down" from special modes to medium-low
-                mode_id = STEADY_IDX;
-                ramp_level = MAX_LEVEL/4;
-            }
-            else {
-                if (ramp_level > 1) {  // solid non-moon mode
-                    // drop by 50% each time
-                    ramp_level = (actual_level >> 1);
-                } else { // Already at the lowest mode
-                    // Turn off the light
-                    poweroff();
-                }
-            }
-            set_level(ramp_level);
-            #ifdef THERMAL_REGULATION
-            target_level = ramp_level;
-            #endif
-            //save_state_wl();  // we didn't actually change the mode
-            *lowbatt_cnt = 0;
-            // Wait before lowering the level again
-            _delay_s();
+    // See if it's been low for a while, and maybe step down
+    if (*lowbatt_cnt >= 8) {
+        // DEBUG: blink on step-down:
+        //set_level(0);  _delay_ms(100);
+
+        if (mode != STEADY_E) {
+            // step "down" from special modes to medium-low
+            mode_id = STEADY_IDX;
+            ramp_level = MAX_LEVEL/4;
         }
+        else {
+            if (ramp_level > 1) {  // solid non-moon mode
+                // drop by 50% each time
+                ramp_level = (actual_level >> 1);
+            } else { // Already at the lowest mode
+                // Turn off the light
+                poweroff();
+            }
+        }
+        set_level(ramp_level);
+        #ifdef THERMAL_REGULATION
+        target_level = ramp_level;
+        #endif
+        *lowbatt_cnt = 0;
 
-        // Make sure conversion is running for next time through
-        // (not relevant with thermal regulation also active)
-        //ADCSRA |= (1 << ADSC);
+        // Wait before lowering the level again
+        _delay_s();
     }
 }
 #endif
